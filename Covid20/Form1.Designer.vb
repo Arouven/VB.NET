9383efabcc14c322c9f6e9606046171e169f55bf
Arouven <aroupoolian@gmail.com>
2020-04-03 12:22:46 +0400

    dynanic video path

--- Covid20/Form1.Designer.vb
+++ Covid20/Form1.Designer.vb
@@ -23,95 +23,70 @@ Partial Class Covid20
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
 		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Covid20))
-		Me.IntroPlayer = New AxWMPLib.AxWindowsMediaPlayer()
+		Me.PanelGameIntro = New System.Windows.Forms.Panel()
 		Me.SkipIntro = New System.Windows.Forms.Button()
-		Me.GamePanel = New System.Windows.Forms.Panel()
 		Me.EnNov = New System.Windows.Forms.Label()
-		Me.BtnEnNovNext = New System.Windows.Forms.Button()
-		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.IntroPlayer = New AxWMPLib.AxWindowsMediaPlayer()
+		Me.PanelGameIntro.SuspendLayout()
 		CType(Me.IntroPlayer, System.ComponentModel.ISupportInitialize).BeginInit()
-		Me.GamePanel.SuspendLayout()
-		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
+		'
+		'PanelGameIntro
+		'
+		Me.PanelGameIntro.Controls.Add(Me.SkipIntro)
+		Me.PanelGameIntro.Controls.Add(Me.EnNov)
+		Me.PanelGameIntro.Controls.Add(Me.IntroPlayer)
+		Me.PanelGameIntro.Location = New System.Drawing.Point(0, 0)
+		Me.PanelGameIntro.Name = "PanelGameIntro"
+		Me.PanelGameIntro.Size = New System.Drawing.Size(800, 507)
+		Me.PanelGameIntro.TabIndex = 3
+		'
+		'SkipIntro
+		'
+		Me.SkipIntro.Location = New System.Drawing.Point(712, 471)
+		Me.SkipIntro.Name = "SkipIntro"
+		Me.SkipIntro.Size = New System.Drawing.Size(75, 23)
+		Me.SkipIntro.TabIndex = 4
+		Me.SkipIntro.Text = "Skip"
+		Me.SkipIntro.UseVisualStyleBackColor = True
+		'
+		'EnNov
+		'
+		Me.EnNov.BackColor = System.Drawing.SystemColors.Control
+		Me.EnNov.Font = New System.Drawing.Font("MV Boli", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.EnNov.ForeColor = System.Drawing.SystemColors.ControlDark
+		Me.EnNov.Location = New System.Drawing.Point(12, 9)
+		Me.EnNov.Name = "EnNov"
+		Me.EnNov.Size = New System.Drawing.Size(775, 74)
+		Me.EnNov.TabIndex = 3
+		Me.EnNov.Text = "En Nov 2019, le novel coronavirus fait son apparition. En Mars 2020 ..."
+		Me.EnNov.TextAlign = System.Drawing.ContentAlignment.TopCenter
 		'
 		'IntroPlayer
 		'
 		Me.IntroPlayer.Enabled = True
-		Me.IntroPlayer.Location = New System.Drawing.Point(-3, 1)
+		Me.IntroPlayer.Location = New System.Drawing.Point(0, 0)
 		Me.IntroPlayer.Name = "IntroPlayer"
 		Me.IntroPlayer.OcxState = CType(resources.GetObject("IntroPlayer.OcxState"), System.Windows.Forms.AxHost.State)
-		Me.IntroPlayer.Size = New System.Drawing.Size(858, 542)
-		Me.IntroPlayer.TabIndex = 0
-		'
-		'SkipIntro
-		'
-		Me.SkipIntro.Location = New System.Drawing.Point(742, 496)
-		Me.SkipIntro.Name = "SkipIntro"
-		Me.SkipIntro.Size = New System.Drawing.Size(75, 23)
-		Me.SkipIntro.TabIndex = 1
-		Me.SkipIntro.Text = "Skip"
-		Me.SkipIntro.UseVisualStyleBackColor = True
-		'
-		'GamePanel
-		'
-		Me.GamePanel.Controls.Add(Me.PictureBox1)
-		Me.GamePanel.Controls.Add(Me.EnNov)
-		Me.GamePanel.Controls.Add(Me.BtnEnNovNext)
-		Me.GamePanel.Location = New System.Drawing.Point(-3, 1)
-		Me.GamePanel.Name = "GamePanel"
-		Me.GamePanel.Size = New System.Drawing.Size(858, 542)
-		Me.GamePanel.TabIndex = 2
-		'
-		'EnNov
-		'
-		Me.EnNov.AutoSize = True
-		Me.EnNov.Font = New System.Drawing.Font("MV Boli", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.EnNov.Location = New System.Drawing.Point(59, 40)
-		Me.EnNov.Name = "EnNov"
-		Me.EnNov.Size = New System.Drawing.Size(761, 34)
-		Me.EnNov.TabIndex = 1
-		Me.EnNov.Text = "En nov 2019, le novel coronavirus fait son apparition."
-		Me.EnNov.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-		'
-		'BtnEnNovNext
-		'
-		Me.BtnEnNovNext.Location = New System.Drawing.Point(755, 495)
-		Me.BtnEnNovNext.Name = "BtnEnNovNext"
-		Me.BtnEnNovNext.Size = New System.Drawing.Size(75, 23)
-		Me.BtnEnNovNext.TabIndex = 0
-		Me.BtnEnNovNext.Text = "Next"
-		Me.BtnEnNovNext.UseVisualStyleBackColor = True
-		'
-		'PictureBox1
-		'
-		Me.PictureBox1.Location = New System.Drawing.Point(65, 90)
-		Me.PictureBox1.Name = "PictureBox1"
-		Me.PictureBox1.Size = New System.Drawing.Size(755, 386)
-		Me.PictureBox1.TabIndex = 2
-		Me.PictureBox1.TabStop = False
+		Me.IntroPlayer.Size = New System.Drawing.Size(800, 507)
+		Me.IntroPlayer.TabIndex = 1
 		'
 		'Covid20
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(853, 544)
-		Me.Controls.Add(Me.GamePanel)
-		Me.Controls.Add(Me.SkipIntro)
-		Me.Controls.Add(Me.IntroPlayer)
+		Me.ClientSize = New System.Drawing.Size(799, 506)
+		Me.Controls.Add(Me.PanelGameIntro)
 		Me.Name = "Covid20"
 		Me.Text = "Covid20"
+		Me.PanelGameIntro.ResumeLayout(False)
 		CType(Me.IntroPlayer, System.ComponentModel.ISupportInitialize).EndInit()
-		Me.GamePanel.ResumeLayout(False)
-		Me.GamePanel.PerformLayout()
-		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 
 	End Sub
 
-	Friend WithEvents IntroPlayer As AxWMPLib.AxWindowsMediaPlayer
+	Friend WithEvents PanelGameIntro As Panel
 	Friend WithEvents SkipIntro As Button
-	Friend WithEvents GamePanel As Panel
-	Friend WithEvents BtnEnNovNext As Button
 	Friend WithEvents EnNov As Label
-	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents IntroPlayer As AxWMPLib.AxWindowsMediaPlayer
 End Class
--- Covid20/Form1.vb
+++ Covid20/Form1.vb
@@ -1,28 +1,13 @@
 ﻿Public Class Covid20
 
     Private Sub Covid20_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        IntroPlayer.Visible = False
-
+        IntroPlayer.URL = AppDomain.CurrentDomain.BaseDirectory.Replace("\bin\Debug\", "\AudioVideo\intro.mp4")
     End Sub
 
     Private Sub SkipIntro_Click(sender As Object, e As EventArgs) Handles SkipIntro.Click
         IntroPlayer.close()
         IntroPlayer.Dispose()
-        GamePanel.Visible = True
-    End Sub
-
-    Private Sub IntroPlayer_Enter(sender As Object, e As EventArgs) Handles IntroPlayer.Enter
-    End Sub
-
-
-    Private Sub Play_Click(sender As Object, e As EventArgs) Handles BtnEnNovNext.Click
-        GamePanel.Visible = False
-
-
-        IntroPlayer.Visible = True
-
-        IntroPlayer.URL = "C:\Users\Arouven\Source\Repos\VB.NET\Covid20\AudioVideo\intro.mp4" 'bizin fr sa url la vin dinamic
-
+        'PanelGameIntro.Visible = True
     End Sub
 
 End Class
